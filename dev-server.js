2d21c6ff6f4dde965a1d82e6f58ab523d7d3b333
Natalizag <55549023+Natalizag@users.noreply.github.com>
2019-10-02 16:07:27 +0300

    add lesson 9

--- dev-server.js
+++ dev-server.js
@@ -4,6 +4,6 @@ console.log('test');
 const app = express();
 app.use(express.static('docs'));
 
-app.listen(8080, function(){
+app.listen(8080, function() {
   console.log('Example app listening on port 8080');
 });
